228a18369ce9dc6f1053663cfe46141aa07dd074
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-21 22:01:25 +0530

    integrate router provider in main.jsx and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <h1>ShoppyGlobe</h1>;
+  return (
+    <div>
+      <h1>ShoppyGlobe</h1>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;

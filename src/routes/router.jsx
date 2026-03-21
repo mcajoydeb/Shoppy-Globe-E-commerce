083cfb5cc26ad2478c5aa2a691bbc7efce428a7c
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-21 22:00:06 +0530

    doen setup routing with createBrowserRouter

---
+++ src/routes/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import ProductDetail from "../pages/ProductDetail";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import NotFound from "../pages/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/product/:id", element: <ProductDetail /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/checkout", element: <Checkout /> },
+    ],
+  },
+]);

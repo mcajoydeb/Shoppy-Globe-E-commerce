f6253652566eb450f6b830f734ff17010e3bf185
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-30 17:14:31 +0530

    to display product list using custom hook

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,21 @@
+import useProducts from "../hooks/useProducts";
+
 function ProductList() {
-  return <h2>Product List</h2>;
+  const { products, loading, error } = useProducts();
+
+  if (loading) return <h3>Loading...</h3>;
+  if (error) return <h3>{error}</h3>;
+
+  return (
+    <div>
+      <h2>Products</h2>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h4>{product.title}</h4>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ProductList;

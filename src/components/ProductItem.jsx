2d92059c0fbe2763bc13f1c0220c37bd6e7a8478
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-30 18:39:19 +0530

    imported link and added to navigation to each products with title to see details page

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,12 +1,17 @@
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/cartSlice";
+import { Link } from "react-router-dom";
 
 function ProductItem({ product }) {
   const dispatch = useDispatch();
 
   return (
     <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
-      <h4>{product.title}</h4>
+      <h4>
+        <Link to={`/product/${product.id}`}>
+                {product.title}
+            </Link>
+        </h4>
       <p>Price: ${product.price}</p>
       <button onClick={() => dispatch(addToCart(product))}>
         Add to Cart

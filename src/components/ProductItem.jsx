2ef169c821985e72bd98bee082113c9d4b5302b3
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-30 17:37:46 +0530

    using redux integrate add to cart functionality

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,16 @@
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
+
 function ProductItem({ product }) {
+  const dispatch = useDispatch();
+
   return (
     <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
       <h4>{product.title}</h4>
-      <p>Price: ${product.price}</p>      
-      <button onClick={() => console.log(product)}>Add to Cart</button>
+      <p>Price: ${product.price}</p>
+      <button onClick={() => dispatch(addToCart(product))}>
+        Add to Cart
+      </button>
     </div>
   );
 }

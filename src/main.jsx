0337974c221b65333a26492493889ac40063f95e
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-30 17:35:56 +0530

    added redux store to main app using

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );

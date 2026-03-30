277c422af94e7e84fcad605592a0fb5974e03c91
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-30 17:20:59 +0530

    adding dummy cart button

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,8 +2,8 @@ function ProductItem({ product }) {
   return (
     <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
       <h4>{product.title}</h4>
-      <p>Price: ${product.price}</p>
-      <button>Add to Cart</button>
+      <p>Price: ${product.price}</p>      
+      <button onClick={() => console.log(product)}>Add to Cart</button>
     </div>
   );
 }

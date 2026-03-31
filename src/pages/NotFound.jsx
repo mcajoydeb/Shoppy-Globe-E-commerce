2cea78d1aa6c851ee69bdfe74e19b9960887ad19
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-03-31 14:20:41 +0530

    Not found page updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ function NotFound() {
     <div className="text-center mt-10">
       <h1 className="text-3xl font-bold text-red-500">404</h1>
       <p className="mt-2">Page not found</p>
-      <Link to="/" className="text-blue-500 underline mt-4 block">
+            <Link to="/" className="text-blue-500 underline mt-4 block">
         Go back to Home
       </Link>
     </div>
